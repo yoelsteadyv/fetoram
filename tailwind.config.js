31ef5906a0f662a4b818975b82f7876c05d80a78
yoelsteadyv <yoelsteadyv@gmail.com>
2024-11-30 02:15:36 +0700

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,24 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,ts,jsx,tsx}",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [
+//     require('daisyui'),
+//   ],
+// }
+
+import daisyui from "daisyui"
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {},
-  },
   plugins: [
-    require('daisyui'),
+    daisyui,
   ],
 }
