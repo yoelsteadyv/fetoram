448ca159cffa71fbe5c5220480b4b9f87b838eee
yoelsteadyv <yoelsteadyv@gmail.com>
2024-11-30 09:08:57 +0700

    .............

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 // import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import api from './api/api.js'
+import api from '../api/api.js'
 
 
 const Register = () => {
