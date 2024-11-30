708b01f1ccc0d7abed3649f2f316078eb628c8f4
yoelsteadyv <yoelsteadyv@gmail.com>
2024-11-30 14:27:34 +0700

    ............

--- src/api/api.js
+++ src/api/api.js
@@ -1,11 +1,16 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://betoram.vercel.app", // URL backend-mu di Vercel
-  withCredentials: true, // Penting untuk mengirim cookie jika backend menggunakan session
+  baseURL: "https://betoram.vercel.app",
+  withCredentials: true, // Penting
   headers: {
     "Content-Type": "application/json",
   },
+});
+
+api.interceptors.request.use((config) => {
+  console.log("Request Config:", config); // Debug
+  return config;
 });
 
 export default api;
